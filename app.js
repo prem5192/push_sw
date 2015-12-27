6cd9a416e96293a54a2c1e3b45e9a0d0b3b4af04
prem5192 <inbox.premkumar@gmail.com>
2015-12-27 12:08:18 +0530

    included ajax call in app.js

--- app.js
+++ app.js
@@ -29,6 +29,38 @@ function subscribe() {
     console.log("push subscription............", pushSubscription)
     sub = pushSubscription;
     console.log('Subscribed! Endpoint:', sub.endpoint);
+    $.ajax({
+      url: "https://android.googleapis.com/gcm/send/",
+      type : "POST",
+      dataType : "json",
+      header: "Access-Control-Allow-Origin: *",
+      header : "Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin, Access-Control-Allow-Headers",
+      contentType : "application/json",
+      authorization : "key=AIzaSyC3ulUzHUwB2E1kbs-o9iOhMtz2a-DnLDQ",
+      data :(sub.endpoint.split("https://android.googleapis.com/gcm/send/"))[1],
+      timeout : 1000000,
+      crossDomain : true,
+      cache : false,
+      async : true,
+
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader ("Authorization", "key=AIzaSyC3ulUzHUwB2E1kbs-o9iOhMtz2a-DnLDQ");
+      },
+
+      success : function(response, e ,xhr){
+        console.log("success.....",response);
+
+      },
+
+      error : function(jqXHR, textStatus, errorThrown){
+        console.log("error....",errorThrown);
+
+      },
+
+      complete : function(response, e ,xhr){
+
+      }
+    });
     subscribeButton.textContent = 'Unsubscribe';
     isSubscribed = true;
   });
