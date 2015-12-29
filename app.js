3e461ede33726da6805ec1a02e1f6137a085e4d3
prem5192 <inbox.premkumar@gmail.com>
2015-12-29 16:09:31 +0530

    removed ajax call

--- app.js
+++ app.js
@@ -29,27 +29,8 @@ function subscribe() {
     console.log("push subscription............", pushSubscription)
     sub = pushSubscription;
     console.log('Subscribed! Endpoint:', sub.endpoint);
-    var endPoint = (sub.endpoint.split("https://android.googleapis.com/gcm/send/"))[1];
-    console.log("endPoint.....", endPoint);
-    $.ajax({
-     url: "https://android.googleapis.com/gcm/send",
-     type : "POST",
-     dataType: "json",
-     contentType: "application/json",
-     crossDomain:true,
-    //header:"Access-Control-Allow-Origin:*",
-     data : JSON.stringify({
-        "registration_ids" : [endPoint]  
-      }),
-     beforeSend : function(req) { 
-        req.setRequestHeader('Authorization', 'key=AIzaSyC3ulUzHUwB2E1kbs-o9iOhMtz2a-DnLDQ'); 
-        req.setRequestHeader('Access-Control-Allow-Origin', '*'); 
-        //req.setRequestHeader('Access-Control-Request-Method', 'POST');
-      },
-     success : function(response, e ,xhr){
-       console.log("success.....",response);
-     }
-   });
+    var register_Id = (sub.endpoint.split("https://android.googleapis.com/gcm/send/"))[1];
+    console.log("register_Id.....", register_Id);
     subscribeButton.textContent = 'Unsubscribe';
     isSubscribed = true;
   });
